32d0ea9f8a368021bc3cf1885007d62f49181138
RICHARD ARRUDA SILVA <richard.asilva16@senacsp.edu.br>
2025-11-24 17:03:20 -0300

    att

---
+++ appIMC/app.js
@@ -0,0 +1,35 @@
+/**
+ * Calculo do IMC
+ * @author Richard Silva 
+ */
+
+let peso, altura, imc
+
+function calcular() {
+    //entrada
+    peso = formIMC.inputPeso.value
+    altura = formIMC.inputAltura.value
+    console.log(peso)
+    console.log(altura)
+    //processamento
+    imc = peso / (altura * altura)
+    //saida
+    formIMC.textIMC.value = `IMC: ${imc.toFixed(2)}`
+    //alterar a imagem em função do resultado
+    if (imc < 18.5) {
+        document.getElementById(`status`).src = "./img/imc2.png"
+    } else if (imc < 24, 9) {
+        document.getElementById(`status`).src = "./img/imc3.png"
+    } else if (imc < 30) {
+        document.getElementById(`status`).src = "./img/imc5.png"
+    } else if (imc < 35) {
+        document.getElementById(`status`).src = "./img/imc6.png"
+    } else if (imc < 39,9) {
+        document.getElementById(`status`).src = "./img/imc7.png"
+    } else if (imc > 40) {
+        document.getElementById(`status`).src = "./img/professorramos.png"
+    }
+}
+function limpar() {
+    document.getElementById(`status`).src="./img/imc.png"
+}
