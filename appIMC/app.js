2e11fb3f6cccae859530cc983205f563d9552a63
ricardiin <richard.foulu@gmail.com>
2025-11-24 19:37:41 -0300

    att

--- appIMC/app.js
+++ appIMC/app.js
@@ -17,17 +17,17 @@ function calcular() {
     formIMC.textIMC.value = `IMC: ${imc.toFixed(2)}`
     //alterar a imagem em função do resultado
     if (imc < 18.5) {
-        document.getElementById(`status`).src = "./img/imc2.png"
-    } else if (imc < 24, 9) {
-        document.getElementById(`status`).src = "./img/imc3.png"
+        document.getElementById("status").src = "./img/imc2.png"
+    } else if (imc < 24.9) {
+        document.getElementById("status").src = "./img/imc3.png"
     } else if (imc < 30) {
-        document.getElementById(`status`).src = "./img/imc5.png"
+        document.getElementById("status").src = "./img/imc4.png"
     } else if (imc < 35) {
-        document.getElementById(`status`).src = "./img/imc6.png"
-    } else if (imc < 39,9) {
-        document.getElementById(`status`).src = "./img/imc7.png"
-    } else if (imc > 40) {
-        document.getElementById(`status`).src = "./img/professorramos.png"
+        document.getElementById("status").src = "./img/imc5.png"
+    } else if (imc < 39.9) {
+        document.getElementById("status").src = "./img/imc6.png"
+    } else {
+        document.getElementById("status").src = "./img/professorramos.png"
     }
 }
 function limpar() {
