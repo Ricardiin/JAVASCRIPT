94c82f609ee92befc60244ce4336962aefd183e5
RICHARD ARRUDA SILVA <richard.asilva16@senacsp.edu.br>
2025-12-01 16:16:31 -0300

    att

---
+++ cartas/app.js
@@ -0,0 +1,10 @@
+/**
+ * Sorteio de uma carta
+ * Exemplo de uso de array para otimizar o código
+ * @author Ricardin
+ */
+
+function sortear() {
+    let nipes = ["♥","♦","♣","♠"]
+    let faces = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+}
